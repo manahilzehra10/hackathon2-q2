c17138e4d5eaeeb2734a68855c957b2357851b1e
Manahil Afaq <163407133+manahilzehra10@users.noreply.github.com>
2024-12-22 12:02:56 +0500

    Update page.tsx

--- src/app/Error/page.tsx
+++ src/app/Error/page.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 function ErrorPage() {
   return (
     <div>
-      {/* <TopBar />
-      <Header/> */}
+      <TopBar />
+      <Header/> 
               <header className="bg-gray-100 py-6 sm:py-8 md:py-12 bg-cover bg-center relative">
           <div className="container mx-auto px-6 text-left">
             <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-blue-900">
@@ -31,7 +31,7 @@ function ErrorPage() {
           </button>
         </Link>
       </div>
-      {/* <Footer /> */}
+     <Footer /> 
     </div>
   );
 }
